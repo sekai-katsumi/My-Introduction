2e36e79c5569e956972518e6371da0bbff54bea6
sekai-katsumi <katsumisekai@gmail.com>
2025-09-26 17:00:49 +0900

    ファイル整理、PWA化準備

---
+++ js/core/serviceWorker.js
@@ -0,0 +1,23 @@
+// sw.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('webar-cache-v1').then((cache) => {
+        return cache.addAll([
+            './',
+            './index.html',
+            './css/style.css',
+            './js/video/videoPlayer.js',
+            './assets/videos/video-01.mp4',
+            // './assets/icons/icon-192.png'
+        ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+        })
+    );
+});
